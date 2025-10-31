387aff189f93da289620d82ebec1b023aaa2d358
henriqueaguiiar <henrique.pacheco@dtidigital.com.br>
2025-10-31 00:49:45 -0300

    feat(repository): Add serviceCandidate and CandidateRepository

--- src/main/java/io/github/henriqueaguiiar/backend_job_manager/api/v1/resources/candidate/CandidateResource.java
+++ src/main/java/io/github/henriqueaguiiar/backend_job_manager/api/v1/resources/candidate/CandidateResource.java
@@ -3,7 +3,9 @@ package io.github.henriqueaguiiar.backend_job_manager.api.v1.resources.candidate
 
 import io.github.henriqueaguiiar.backend_job_manager.api.v1.resources.dto.CandidateDTO;
 import io.github.henriqueaguiiar.backend_job_manager.domain.entity.Candidate;
+import io.github.henriqueaguiiar.backend_job_manager.domain.services.impl.CandidateServiceImpl;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateResource {
 
 
+    private final CandidateServiceImpl candidateService;
+
+    @Autowired
+    public CandidateResource(CandidateServiceImpl candidateService) {
+        this.candidateService = candidateService;
+    }
 
     @PostMapping
     public ResponseEntity<Candidate> create(@Valid @RequestBody CandidateDTO candidateDTO) {
         Candidate candidate = new Candidate(candidateDTO);
+        candidateService.createCandidate( candidate );
         return ResponseEntity.ok().body(candidate);
     }
 
---
+++ src/main/java/io/github/henriqueaguiiar/backend_job_manager/domain/repository/CandidateRepository.java
@@ -0,0 +1,12 @@
+package io.github.henriqueaguiiar.backend_job_manager.domain.repository;
+
+import io.github.henriqueaguiiar.backend_job_manager.domain.entity.Candidate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface CandidateRepository extends JpaRepository<Candidate, UUID> {
+}
