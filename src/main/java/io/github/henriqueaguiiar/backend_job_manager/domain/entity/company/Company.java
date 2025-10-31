67d0a87c3cb66ddafda671b2f9a699cfc9051448
henriqueaguiiar <henrique.pacheco@dtidigital.com.br>
2025-10-31 12:32:29 -0300

    feat(Entity) created entity company

---
+++ src/main/java/io/github/henriqueaguiiar/backend_job_manager/domain/entity/company/Company.java
@@ -0,0 +1,53 @@
+package io.github.henriqueaguiiar.backend_job_manager.domain.entity.company;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Company implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    @Pattern(regexp = "^(?!\\s*$).+" , message = "Username cannot be blank")
+    private String username;
+    @Email(message = "Email should be valid")
+    private String email;
+    @Length(min = 10, max = 100, message = "Password must be between 10 and 100 characters")
+    private String password;
+    private String website;
+    private String description;
+    private Instant createdAt;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Company company = (Company) o;
+        return Objects.equals(id, company.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
