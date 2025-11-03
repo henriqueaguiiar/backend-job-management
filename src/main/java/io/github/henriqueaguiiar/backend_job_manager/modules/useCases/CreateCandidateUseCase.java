66102d6bbdc2f5a6a0c2721e35ebe62db6cb32da
henriqueaguiiar <henrique.pacheco@dtidigital.com.br>
2025-11-02 23:17:47 -0300

    feat(security): password hash encoder candidate Entity

--- src/main/java/io/github/henriqueaguiiar/backend_job_manager/modules/useCases/CreateCandidateUseCase.java
+++ src/main/java/io/github/henriqueaguiiar/backend_job_manager/modules/useCases/CreateCandidateUseCase.java
@@ -4,19 +4,31 @@ import io.github.henriqueaguiiar.backend_job_manager.exceptions.UserFoundExcepti
 import io.github.henriqueaguiiar.backend_job_manager.modules.entities.CandidateEntity;
 import io.github.henriqueaguiiar.backend_job_manager.modules.repository.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class CreateCandidateUseCase {
-    @Autowired
+
     private CandidateRepository candidateRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public CreateCandidateUseCase(CandidateRepository candidateRepository, PasswordEncoder passwordEncoder) {
+        this.candidateRepository = candidateRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(),
                 candidateEntity.getEmail()).ifPresent(user -> {
             throw new UserFoundException();
         });
+
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
