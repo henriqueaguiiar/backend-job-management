31a7b8eddddbed4752c3ae782e7a04590c01904e
henriqueaguiiar <henrique.pacheco@dtidigital.com.br>
2025-11-04 17:15:02 -0300

    Add banco de dados para testes H2

--- src/main/java/io/github/henriqueaguiiar/backend_job_manager/dto/CreateJobDTO.java
+++ src/main/java/io/github/henriqueaguiiar/backend_job_manager/dto/CreateJobDTO.java
@@ -1,9 +1,15 @@
 package io.github.henriqueaguiiar.backend_job_manager.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateJobDTO {
 
     @Schema(example = "Vaga para pessoa desenvolvedora", requiredMode = Schema.RequiredMode.REQUIRED)
